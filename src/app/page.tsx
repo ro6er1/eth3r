78339ad323e0504b9c8c67f3250349b2d776941a
Roger Taylor <roger@eth3r.co.uk>
2023-09-04 15:49:10 +0100

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 "use client";
-import React, { useState, useEffect } from 'react'
 import ReactShadowRoot from 'react-shadow-root';
-
-import Image from 'next/image'
-import styles from './page.module.css'
 import Squares from './squares'
 import Logo from './logo'
 
@@ -102,101 +98,9 @@ export default function Home() {
         </p>
         
 
-        <sup><i>This small site was made using HTML, JS, small SVG art, SCSS and http-server</i></sup>
-        <sup><i>My codepen for this animated background: <a href="https://codepen.io/ro6er/pen/xxQPvwY" target="_blank">https://codepen.io/ro6er/pen/xxQPvwY</a></i></sup>
+        <sup><i>This small site was made using NextJs and Web ComponentsL <a href="https://github.com/ro6er1/eth3r" target="_blank">https://github.com/ro6er1/eth3r</a></i></sup>
+        <sup><i>Codepen for a JS version of the animated background: <a href="https://codepen.io/ro6er/pen/xxQPvwY" target="_blank">https://codepen.io/ro6er/pen/xxQPvwY</a></i></sup>
       </main>
     </ReactShadowRoot>
-
-    // <main className={styles.main}>
-
-    //   <Squares />
-    //   yo
-    //   {/* <div className={styles.description}>
-    //     <p>
-    //       Get started by editing&nbsp; dasd asda sdasdasdasd
-    //       <code className={styles.code}>src/app/page.tsx</code>
-    //     </p>
-    //     <div>
-    //       <a
-    //         href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         By{' '}
-    //         <Image
-    //           src="/vercel.svg"
-    //           alt="Vercel Logo"
-    //           className={styles.vercelLogo}
-    //           width={100}
-    //           height={24}
-    //           priority
-    //         />
-    //       </a>
-    //     </div>
-    //   </div>
-
-    //   <div className={styles.center}>
-    //     <Image
-    //       className={styles.logo}
-    //       src="/next.svg"
-    //       alt="Next.js Logo"
-    //       width={180}
-    //       height={37}
-    //       priority
-    //     />
-    //   </div>
-
-    //   <div className={styles.grid}>
-    //     <a
-    //       href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className={styles.card}
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2>
-    //         Docs <span>-&gt;</span>
-    //       </h2>
-    //       <p>Find in-depth information about Next.js features and API.</p>
-    //     </a>
-
-    //     <a
-    //       href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className={styles.card}
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2>
-    //         Learn <span>-&gt;</span>
-    //       </h2>
-    //       <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className={styles.card}
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2>
-    //         Templates <span>-&gt;</span>
-    //       </h2>
-    //       <p>Explore the Next.js 13 playground.</p>
-    //     </a>
-
-    //     <a
-    //       href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-    //       className={styles.card}
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       <h2>
-    //         Deploy <span>-&gt;</span>
-    //       </h2>
-    //       <p>
-    //         Instantly deploy your Next.js site to a shareable URL with Vercel.
-    //       </p>
-    //     </a>
-    //   </div> */}
-    // </main>
   )
 }
